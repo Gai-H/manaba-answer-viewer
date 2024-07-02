eb21337e39a6d58bcf0835732c1557e455dcda1f
Gaishi Hirota <mail@gaishi.dev>
2024-07-02 11:52:42 +0900

    feat: setup header

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite';
-import monkey from 'vite-plugin-monkey';
+import { defineConfig } from "vite"
+import monkey from "vite-plugin-monkey"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     monkey({
-      entry: 'src/main.ts',
+      entry: "src/main.ts",
       userscript: {
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        name: "Manaba Answer Viewer",
+        namespace: "https://github.com/Gai-H/manaba-answer-viewer",
+        version: "0.0",
+        description: "manaba の小テストの回答と正解を横並びで表示します。",
+        include: [/^https:\/\/manaba\.tsukuba\.ac\.jp\/local\/course_\d+_query_\d+\/?.*$/],
+        exclude: [/^https:\/\/manaba\.tsukuba\.ac\.jp\/local\/course_\d+_query_\d+\/?.*answer=1.*$/],
+        author: "Gai",
+        source: "https://github.com/Gai-H/manaba-answer-viewer",
+        license: "MIT",
       },
     }),
   ],
-});
+})
